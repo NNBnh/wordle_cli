5ed2ba911daedd354915a142241e8588ef649bc8
NNB <nnbnh@protonmail.com>
2022-01-27 11:03:15 +0700

    modified:   lib/wordle_cli.rb 	modified:   lib/wordle_cli/version.rb 	new file:   lib/wordle_cli/word_list.rb 	new file:   wordle_cli-1.0.0.gem 	modified:   wordle_cli.gemspec

--- lib/wordle_cli.rb
+++ lib/wordle_cli.rb
@@ -1,8 +1,55 @@
 # frozen_string_literal: true
 
+# require_relative "wordle_cli/word_list"
 require_relative "wordle_cli/version"
 
-module WordleCli
+module Wordle
   class Error < StandardError; end
-  # Your code goes here...
+
+  @@word_list = %w[apple linux helix super upper under hello cat dog fish begin end]
+
+  def self.start(word = @@word_list.select { |word| word.length == 5 } .sample, guess: 6)
+    word_length = word.length
+
+    if word.is_a?(Integer)
+      word_length = word
+      word = @@word_list.select { |word| word.length == word_length } .sample
+    end
+
+    guess_word_list = @@word_list.select { |word| word.length == word_length }
+
+    guess.times do |guess_count|
+      padding = guess.to_s.length + 1
+      guess_word = nil
+
+      loop do
+        print "\033[1m", ">".ljust(padding), "\033[m"
+        guess_word = gets.chomp.downcase
+
+        break if guess_word_list.include?(guess_word)
+
+        print "\033[A\033[?7l", " " * guess_word.size * 4, "\033[?7h\n\033[A"
+      end
+
+      print "\033[A\033[37m", (guess_count + 1).to_s.ljust(padding), "\033[m"
+
+      guess_word.chars.each_with_index do |char, index|
+        if char == word[index]
+          print "\033[0;1;30;102m"
+        elsif word.include? char
+          print "\033[0;30;103m"
+        else
+          print "\033[0;100m"
+        end
+
+        print "#{char}"
+      end
+
+      puts "\033[m"
+
+      return if guess_word == word
+    end
+
+    return word
+  end
 end
--- wordle_cli.gemspec
+++ wordle_cli.gemspec
@@ -8,15 +8,14 @@ Gem::Specification.new do |spec|
   spec.authors = ["NNB"]
   spec.email = ["nnbnh@protonmail.com"]
 
-  spec.summary = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description = "TODO: Write a longer description or delete this line."
+  spec.summary = "A simple CLI Wordle game."
   spec.homepage = "https://github.com/NNBnh/wordle_cli"
   spec.license = "MIT"
   spec.required_ruby_version = ">= 2.6.0"
 
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = "https://github.com/NNBnh/wordle_cli"
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["changelog_uri"] = "https://github.com/NNBnh/wordle_cli/blob/main/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
